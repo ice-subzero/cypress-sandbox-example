2a5ad5d3825357024c59f57c997f9388ecd904c9
sub-zero <113250499+ice-subzero@users.noreply.github.com>
2024-05-30 11:45:37 +0700

    Update login-page.ts
    doc: add naming convention of POM.

--- cypress/pages/login-page.ts
+++ cypress/pages/login-page.ts
@@ -1,3 +1,28 @@
+//   Naming convention 
+//   +----------+----------------------------+--------+-----------------+
+//   | Category |      UI/Control type       | Prefix |     Example     |
+//   +----------+----------------------------+--------+-----------------+
+//   | Basic    | Button                     | btn    | btnExit         |
+//   | Basic    | Check box                  | chk    | chkReadOnly     |
+//   | Basic    | Combo box                  | cbo    | cboEnglish      |
+//   | Basic    | Common dialog              | dlg    | dlgFileOpen     |
+//   | Basic    | Date picker                | dtp    | dtpPublished    |
+//   | Basic    | Dropdown List / Select tag | ddl    | ddlCountry      |
+//   | Basic    | Form                       | frm    | frmEntry        |
+//   | Basic    | Frame                      | fra    | fraLanguage     |
+//   | Basic    | Image                      | img    | imgIcon         |
+//   | Basic    | Label                      | lbl    | lblHelpMessage  |
+//   | Basic    | Links/Anchor Tags          | lnk    | lnkForgotPwd    |
+//   | Basic    | List box                   | lst    | lstPolicyCodes  |
+//   | Basic    | Menu                       | mnu    | mnuFileOpen     |
+//   | Basic    | Text Area                  | txa    | txaDescription  |
+//   | Basic    | Text box                   | txt    | txtLastName     |
+//   | Complex  | Panel/Fieldset             | pnl    | pnlGroup        |
+//   | Complex  | Slider                     | sld    | sldScale        |
+//   | Complex  | StatusBar                  | sta    | staDateTime     |
+//   | Complex  | Toolbar                    | tlb    | tlbActions      |
+//   +----------+----------------------------+--------+-----------------+
+
 const loginComponent = {
     txtInputUsername: '//input[@id="username"]',
     txtInputPassword: '//input[@id="password"]',
